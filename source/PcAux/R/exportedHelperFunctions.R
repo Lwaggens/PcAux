203cb2e06ce63e7e2c551cbccdf974ea0388e7dc
Lwaggens <39630567+Lwaggens@users.noreply.github.com>
2020-04-15 15:00:18 -0500

    Update exportedHelperFunctions.R

--- source/PcAux/R/exportedHelperFunctions.R
+++ source/PcAux/R/exportedHelperFunctions.R
@@ -245,7 +245,7 @@ makePredMatrix <- function(mergedData,
 ## New Predictor Matrix Method to ensure no PCs below a certain threshhold are used
 ## Also protects against nPredictors > nObservations iteratively
 pcQuickPred <- function(data,
-                        mincor = micPcCor,
+                        mincor = minPcCor,
                         nLinear = NULL,
                         nNonLinear = NULL ) {
 
